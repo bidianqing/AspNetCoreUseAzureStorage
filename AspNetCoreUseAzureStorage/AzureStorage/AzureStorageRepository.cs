4402792feb32f7aa78039dfe8e320ac04441643d
毕殿卿 <bdq123@outlook.com>
2020-11-12 00:10:34 +0800

    上传进度

--- AspNetCoreUseAzureStorage/AzureStorage/AzureStorageRepository.cs
+++ AspNetCoreUseAzureStorage/AzureStorage/AzureStorageRepository.cs
@@ -29,11 +29,29 @@ namespace AspNetCoreUseAzureStorage.AzureStorage
 
             BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
 
+            var l = file.Length;
+            Console.WriteLine($"文件大小{l}");
             using var content = file.OpenReadStream();
             await blobClient.UploadAsync(content, new BlobHttpHeaders
             {
                 ContentType = file.ContentType
-            });
+            },null,null,new Progress(l));
+        }
+    }
+
+    public class Progress : IProgress<long>
+    {
+        private long _length { get; set; }
+        public Progress(long length)
+        {
+            _length = length;
+        }
+
+        public void Report(long value)
+        {
+            var ratio = value * 1.0 / _length * 100;
+
+            Console.WriteLine(value + "进度为" + Math.Round(ratio, 1) + "%");
         }
     }
 }
